210ddaea967ed8e6d6b49ee0ba7e2f43900aa828
Dan Kempster <me@dankempster.co.uk>
2013-01-07 15:22:32 +0000

    Add new Contact model interface - the defined method stubs are required by EmailContactListener

---
+++ Model/Contact.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace FrequenceWeb\Bundle\ContactBundle\Model;
+
+/**
+ * Class attached to the contact form. Represents data from it.
+ * You can extend it, and / or make it an entity or a document.
+ *
+ * @author Yohan Giarelli <yohan@giarel.li>
+ */
+class Contact
+{
+    /**
+     * @return string
+     */
+    public function getEmail();
+
+    /**
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * Returns data that can be injected in the translating message subject
+     *
+     * @return array
+     */
+    public function toTranslateArray();
+}
