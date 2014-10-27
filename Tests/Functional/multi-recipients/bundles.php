7c0e13d3a32c129526643a98a2d2a3a7ffb999ec
Dan Kempster <dev@dankempster.co.uk>
2014-10-27 21:55:01 +0000

    Extend WebTestCase to allow for different config between tests.

---
+++ Tests/Functional/multi-recipients/bundles.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+    new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+    new \Symfony\Bundle\TwigBundle\TwigBundle(),
+    new \FrequenceWeb\Bundle\ContactBundle\FrequenceWebContactBundle(),
+);
